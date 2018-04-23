1d40ba7deb6bb01c23c64db5ba38c9a4906ebc4c
puru0019 <purushothama.0019@gmail.com>
2018-04-23 20:48:34 +0530

    create todo

--- server.js
+++ server.js
@@ -1,35 +1,21 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 var PORT = process.env.PORT || 3000;
 
-var todos = [
-	{
-		"id":1,
-		"description": "Need to book ticket",
-		"visible": true
-	},
-	{
-		"id":2,
-		"description": "pay power bill",
-		"visible": false
-	},
-	{
-		"id":3,
-		"description": "buy mac laptop",
-		"visible":true
-	}
-]
+var todos = [];
+var todoNextId = 1;
 
 
 var middleware = {
 	requireAuthentication: function(req,res,next){
-		console.log("entered in private route");
 		next();
 	}
 }
 
 app.use(middleware.requireAuthentication);
+app.use(bodyParser.json());
 
 app.get('/about',function(req,res){
 	res.send('About Page');
@@ -54,6 +40,13 @@ app.get('/todos/:id',function(req,res){
 		res.status(404).send();
 	}
 
+});
+
+app.post('/todos',function(req,res){
+	var body = req.body;
+	body.id = todoNextId++;
+	todos.push(body);
+	res.json(body);
 });
 
 app.use(express.static(__dirname+'/public'));
